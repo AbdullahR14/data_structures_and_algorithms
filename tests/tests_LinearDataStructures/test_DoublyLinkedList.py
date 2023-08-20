236ee929c5f4c7dc22863363e7dcf17ad770adb2
Abdullah Rawat <abdullahrawat14@gmail.com>
2023-08-20 21:47:24 +0100

    Altered Linked List test files to use class based testing for better orgsnisation, added remove single node tests in DoublyLinkedList test file

--- tests/tests_LinearDataStructures/test_DoublyLinkedList.py
+++ tests/tests_LinearDataStructures/test_DoublyLinkedList.py
@@ -2,15 +2,15 @@ import pytest
 from LinearDataStructures.DoublyLinkedList import DoublyLinkedList
 
 
-# Fixture to create an empty DoublyLinkedList instance
 @pytest.fixture
 def empty_linkedlist():
+    """ Fixture to create an empty DoublyLinkedList instance """
     return DoublyLinkedList()
 
 
-# Fixture to create a DoublyLinkedList instance with some nodes
 @pytest.fixture
 def linkedlist_with_nodes():
+    """ Fixture to create a DoublyLinkedList instance with some nodes """
     ll = DoublyLinkedList()
     ll.add_node_to_back(1)
     ll.add_node_to_back(2)
@@ -19,46 +19,60 @@ def linkedlist_with_nodes():
     return ll
 
 
-def test_add_node_to_back(empty_linkedlist):
-    empty_linkedlist.add_node_to_back(1)
-    empty_linkedlist.add_node_to_back(2)
-    assert empty_linkedlist.__str__() == "1, 2"
+class TestAddNode:
+    def test_add_node_to_back(self, empty_linkedlist):
+        empty_linkedlist.add_node_to_back(1)
+        empty_linkedlist.add_node_to_back(2)
+        assert empty_linkedlist.__str__() == "1, 2"
+
+    def test_add_node_to_front(self, empty_linkedlist):
+        empty_linkedlist.add_node_to_front(1)
+        empty_linkedlist.add_node_to_front(2)
+        assert empty_linkedlist.__str__() == "2, 1"
 
 
-def test_add_node_to_front(empty_linkedlist):
-    empty_linkedlist.add_node_to_front(1)
-    empty_linkedlist.add_node_to_front(2)
-    assert empty_linkedlist.__str__() == "2, 1"
+class TestRemoveSingleNode:
+    def test_remove_single_node_instance(self, linkedlist_with_nodes):
+        linkedlist_with_nodes.remove_single_node_instance(3)
+        assert linkedlist_with_nodes.__str__() == "1, 2, 2"
+
+    def test_remove_single_node_instance_empty(self, empty_linkedlist):
+        with pytest.raises(ValueError):
+            empty_linkedlist.remove_single_node_instance(1)
+
+    def test_remove_single_node_instance_head(self, linkedlist_with_nodes):
+        linkedlist_with_nodes.remove_single_node_instance(1)
+        assert linkedlist_with_nodes.__str__() == "2, 2, 3"
+
+    def test_remove_single_node_instance_tail(self, linkedlist_with_nodes):
+        linkedlist_with_nodes.remove_single_node_instance(3)
+        assert linkedlist_with_nodes.__str__() == "1, 2, 2"
+
+    def test_remove_single_node_instance_not_found(self, linkedlist_with_nodes):
+        with pytest.raises(ValueError):
+            linkedlist_with_nodes.remove_single_node_instance(100)
 
 
-def test_remove_single_node_instance(linkedlist_with_nodes):
-    linkedlist_with_nodes.remove_single_node_instance(3)
-    assert linkedlist_with_nodes.__str__() == "1, 2, 2"
+class TestRemoveAllNodeInstances:
+    def test_remove_all_instances(self, linkedlist_with_nodes):
+        removed_nodes = linkedlist_with_nodes.remove_all_instances(2)
+        assert linkedlist_with_nodes.__str__() == "1, 3"
+        assert [node.value for node in removed_nodes.values()] == [2, 2]
 
+    def test_remove_all_instances_empty(self, empty_linkedlist):
+        with pytest.raises(ValueError):
+            empty_linkedlist.remove_all_instances(2)
 
-def test_remove_all_instances(linkedlist_with_nodes):
-    removed_nodes = linkedlist_with_nodes.remove_all_instances(2)
-    assert linkedlist_with_nodes.__str__() == "1, 3"
-    assert [node.value for node in removed_nodes.values()] == [2, 2]
+    def test_remove_all_instances_head(self, linkedlist_with_nodes):
+        removed_nodes = linkedlist_with_nodes.remove_all_instances(1)
+        assert linkedlist_with_nodes.__str__() == "2, 2, 3"
+        assert [node.value for node in removed_nodes.values()] == [1]
 
+    def test_remove_all_instances_tail(self, linkedlist_with_nodes):
+        removed_nodes = linkedlist_with_nodes.remove_all_instances(3)
+        assert linkedlist_with_nodes.__str__() == "1, 2, 2"
+        assert [node.value for node in removed_nodes.values()] == [3]
 
-def test_remove_all_instances_empty(empty_linkedlist):
-    with pytest.raises(ValueError):
-        empty_linkedlist.remove_all_instances(2)
-
-
-def test_remove_all_instances_head(linkedlist_with_nodes):
-    removed_nodes = linkedlist_with_nodes.remove_all_instances(1)
-    assert linkedlist_with_nodes.__str__() == "2, 2, 3"
-    assert [node.value for node in removed_nodes.values()] == [1]
-
-
-def test_remove_all_instances_tail(linkedlist_with_nodes):
-    removed_nodes = linkedlist_with_nodes.remove_all_instances(3)
-    assert linkedlist_with_nodes.__str__() == "1, 2, 2"
-    assert [node.value for node in removed_nodes.values()] == [3]
-
-
-def test_remove_all_instances_not_found(linkedlist_with_nodes):
-    with pytest.raises(ValueError):
-        linkedlist_with_nodes.remove_all_instances(4)
+    def test_remove_all_instances_not_found(self, linkedlist_with_nodes):
+        with pytest.raises(ValueError):
+            linkedlist_with_nodes.remove_all_instances(4)
--- tests/tests_LinearDataStructures/test_LinkedList.py
+++ tests/tests_LinearDataStructures/test_LinkedList.py
@@ -2,15 +2,15 @@ import pytest
 from LinearDataStructures.LinkedList import LinkedList
 
 
-# Fixture to create an empty LinkedList instance
 @pytest.fixture
 def empty_linkedlist():
+    """ Fixture to create an empty LinkedList instance """
     return LinkedList()
 
 
-# Fixture to create a LinkedList instance with some nodes
 @pytest.fixture
 def linkedlist_with_nodes():
+    """ Fixture to create a LinkedList instance with some nodes """
     ll = LinkedList()
     ll.add_node(1)
     ll.add_node(2)
@@ -18,47 +18,44 @@ def linkedlist_with_nodes():
     return ll
 
 
-def test_add_node_to_empty_list(empty_linkedlist):
-    empty_linkedlist.add_node(1)
-    assert empty_linkedlist.__str__() == "1"
+class TestAddNode:
+    def test_add_node_to_empty_list(self, empty_linkedlist):
+        empty_linkedlist.add_node(1)
+        assert empty_linkedlist.__str__() == "1"
+
+    def test_add_node_to_non_empty_list(self, linkedlist_with_nodes):
+        linkedlist_with_nodes.add_node(4)
+        assert linkedlist_with_nodes.__str__() == "1, 2, 3, 4"
 
 
-def test_add_node_to_non_empty_list(linkedlist_with_nodes):
-    linkedlist_with_nodes.add_node(4)
-    assert linkedlist_with_nodes.__str__() == "1, 2, 3, 4"
+class TestRemoveNode:
+    def test_remove_node_head(self, linkedlist_with_nodes):
+        removed_node = linkedlist_with_nodes.remove_node(1)
+        assert linkedlist_with_nodes.__str__() == "2, 3"
+        assert removed_node.value == 1
+
+    def test_remove_node_middle(self, linkedlist_with_nodes):
+        removed_node = linkedlist_with_nodes.remove_node(2)
+        assert linkedlist_with_nodes.__str__() == "1, 3"
+        assert removed_node.value == 2
+
+    def test_remove_node_tail(self, linkedlist_with_nodes):
+        removed_node = linkedlist_with_nodes.remove_node(3)
+        assert linkedlist_with_nodes.__str__() == "1, 2"
+        assert removed_node.value == 3
+
+    def test_remove_node_not_found(self, linkedlist_with_nodes):
+        with pytest.raises(ValueError):
+            linkedlist_with_nodes.remove_node(100)
+
+    def test_remove_node_empty(self, empty_linkedlist):
+        with pytest.raises(ValueError):
+            empty_linkedlist.remove_node(1)
 
 
-def test_remove_node_head(linkedlist_with_nodes):
-    removed_node = linkedlist_with_nodes.remove_node(1)
-    assert linkedlist_with_nodes.__str__() == "2, 3"
-    assert removed_node.value == 1
+class TestStringRepresentation:
+    def test_string_representation_empty(self, empty_linkedlist):
+        assert empty_linkedlist.__str__() == "Empty List"
 
-
-def test_remove_node_middle(linkedlist_with_nodes):
-    removed_node = linkedlist_with_nodes.remove_node(2)
-    assert linkedlist_with_nodes.__str__() == "1, 3"
-    assert removed_node.value == 2
-
-
-def test_remove_node_tail(linkedlist_with_nodes):
-    removed_node = linkedlist_with_nodes.remove_node(3)
-    assert linkedlist_with_nodes.__str__() == "1, 2"
-    assert removed_node.value == 3
-
-
-def test_remove_node_not_found(linkedlist_with_nodes):
-    with pytest.raises(ValueError):
-        linkedlist_with_nodes.remove_node(100)
-
-
-def test_remove_node_empty(empty_linkedlist):
-    with pytest.raises(ValueError):
-        empty_linkedlist.remove_node(1)
-
-
-def test_string_representation_empty(empty_linkedlist):
-    assert empty_linkedlist.__str__() == "Empty List"
-
-
-def test_string_representation_non_empty(linkedlist_with_nodes):
-    assert linkedlist_with_nodes.__str__() == "1, 2, 3"
+    def test_string_representation_non_empty(self, linkedlist_with_nodes):
+        assert linkedlist_with_nodes.__str__() == "1, 2, 3"
