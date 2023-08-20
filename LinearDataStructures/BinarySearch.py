94f296e7a80a90ae6e85079d03751da1f43f92da
Abdullah Rawat <abdullahrawat14@gmail.com>
2023-08-21 00:02:46 +0100

    Added code to represent Binary search via recursion and iteration implementations, along with parametrised tests for both

---
+++ LinearDataStructures/BinarySearch.py
@@ -0,0 +1,74 @@
+from typing import Iterable
+
+
+def binary_search_recursive(search_iterable: Iterable[int | float], left_pointer: int, right_pointer: int,
+                            search_value: int | float) -> int:
+    """
+    Recursive Binary Search implementation
+
+    Time Complexity: O(log n)
+    Space Complexity: O(1)
+
+    :param search_iterable: An iterable to search through
+    :param left_pointer: The left pointer of the search range
+    :param right_pointer: The right pointer of the search range
+    :param search_value: The value to search for
+    :return: index of the search_value in the search_iterable
+    :raises: ValueError if the search_value is not found in the search_iterable
+    """
+    # Empty List
+    if left_pointer > right_pointer:
+        raise ValueError("Value not found in iterable")
+
+    # Find the middle of the list
+    mid_pointer = (left_pointer + right_pointer) // 2
+
+    # Check if the middle value is the search value
+    if search_iterable[mid_pointer] == search_value:
+        return mid_pointer
+
+    # Check if the middle value is greater than the search value
+    if search_iterable[mid_pointer] > search_value:
+        # return recursive call with the right pointer set to the middle pointer - 1
+        return binary_search_recursive(search_iterable, left_pointer, mid_pointer - 1, search_value)
+
+    # Check if the middle value is less than the search value
+    if search_iterable[mid_pointer] < search_value:
+        # return recursive call with the left pointer set to the middle pointer + 1
+        return binary_search_recursive(search_iterable, mid_pointer + 1, right_pointer, search_value)
+
+
+def binary_search_iterative(search_iterable: Iterable[int | float], search_value: int | float) -> int:
+    """
+    Iterative Binary Search implementation
+
+    Time Complexity: O(log n)
+    Space Complexity: O(1)
+
+    :param search_iterable: An iterable to search through
+    :param search_value: The value to search for
+    :return: index of the search_value in the search_iterable
+    :raises: ValueError if the search_value is not found in the search_iterable
+    """
+    left_pointer = 0
+    right_pointer = len(search_iterable) - 1
+
+    while left_pointer <= right_pointer:
+        # Find the middle of the list
+        mid_pointer = (left_pointer + right_pointer) // 2
+
+        # Check if the middle value is the search value
+        if search_iterable[mid_pointer] == search_value:
+            return mid_pointer
+
+        # Check if the middle value is greater than the search value
+        if search_iterable[mid_pointer] > search_value:
+            # set the right pointer to the middle pointer - 1
+            right_pointer = mid_pointer - 1
+
+        # Check if the middle value is less than the search value
+        if search_iterable[mid_pointer] < search_value:
+            # set the left pointer to the middle pointer + 1
+            left_pointer = mid_pointer + 1
+
+    raise ValueError("Value not found in iterable")
---
+++ tests/tests_LinearDataStructures/test_BinarySearch.py
@@ -0,0 +1,43 @@
+import pytest
+from LinearDataStructures.BinarySearch import binary_search_recursive, binary_search_iterative
+
+
+@pytest.mark.parametrize(
+    "iterable, search_value, result",
+    [
+        ([1, 2, 3, 4, 5], 1, 0),       # Test left most value
+        ([1, 2, 3, 4, 5], 3, 2),       # Test middle value
+        ([1, 2, 3, 4, 5], 5, 4),       # Test right most value
+        ([1, 2, 3, 3], 3, 2),          # Test list with duplicate values
+        ([1, 1, 1], 1, 1),             # Test list that only contains duplicates
+        ([-3, -2, -1], -1, 2),         # Test list that contains negative value
+        ([1.1, 2.2, 3.3], 1.1, 0),     # Test with float values
+        ([-3, 0, 1, 2.2, 3], 2.2, 3),  # Test with multiple value types
+        ([42], 42, 0),                 # Test list with single value
+    ]
+)
+class TestBinarySearchSuccess:
+    def test_binary_search_recursive(self, iterable, search_value, result):
+        assert binary_search_recursive(iterable, 0, len(iterable) - 1, search_value) == result
+
+    def test_binary_search_iterative(self, iterable, search_value, result):
+        assert binary_search_iterative(iterable, search_value) == result
+
+
+@pytest.mark.parametrize(
+    "iterable, search_value",
+    [
+        ([1, 2, 3, 4, 5], 6),  # Test search value not in the list
+        ([], 42),              # Test with an empty list
+        ([1, 2, 3, 4, 5], 0),  # Test search value less than the minimum
+        ([1, 2, 3, 4, 5], 6),  # Test search value greater than the maximum
+    ]
+)
+class TestBinarySearchFailure:
+    def test_binary_search_recursive_empty(self, iterable, search_value):
+        with pytest.raises(ValueError):
+            binary_search_recursive(iterable, 0, len(iterable) - 1, search_value)
+
+    def test_binary_search_iterative_empty(self, iterable, search_value):
+        with pytest.raises(ValueError):
+            binary_search_iterative(iterable, search_value)
